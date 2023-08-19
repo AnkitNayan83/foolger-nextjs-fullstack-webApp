632f22f93168565be50e0a117acefc81247eae0d
Ankit Nayan <ankitnayan83@gmail.com>
2023-08-19 17:02:07 +0530

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
-import { useState } from "react";
 import Footer from "@/components/Footer";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
