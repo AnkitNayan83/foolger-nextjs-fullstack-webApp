f62ab803eca3f052ea2c24ae856a2e5c561a3021
Ankit Nayan <ankitnayan83@gmail.com>
2023-08-19 17:01:33 +0530

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import { useState } from "react";
 import Footer from "@/components/Footer";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
